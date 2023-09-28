5a2c6aef57b15ae1acb8737ca02c3550420c13f4
nghihai <nghi84072@gmail.com>
2023-09-28 00:11:28 -0700

    step 2 and step 3

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//STEP 1
+let someMonth
+function theMoth(){}
+let currentMonth
+let summerMonth
+let myLibraryFunction
+//STEP 2 + STEP 3
+3.9 //numeric literal
+'This is a string' //string literal
+false //boolean literal 
+null //null literal
+
+3 * 2 + (10/2) //first complex literal 
+7 + 9 + (5 - 2) //second complex literal 
