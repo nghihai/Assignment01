d8e5fcb4cb692eb8b4d1e7ea98df266523c88262
nghihai <nghi84072@gmail.com>
2023-09-28 22:18:54 -0700

    step 11

--- js/script.js
+++ js/script.js
@@ -35,4 +35,34 @@ console.log(boolStr)
 let numBool = 3 + false 
 console.log(numBool)
 
+//step 8 
+//The string is not valid 
+let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"' //fixed string 
+console.log(someString)
+
+//step 9 
+let nullVar = null 
+console.log(nullVar)
+let undefVar //undefined variable 
+console.log(undefVar)
+
+//step 10 
+console.log(typeof(boolStr)) //string 
+console.log(typeof(numBool)) //number 
+console.log(typeof(blnMayWeContactYou)) //bool
+
+//create object variable 
+let classInfo = {code: "comp690", class: "javascript"}
+console.log(typeof(classInfo)) //object
+console.log(typeof(undefVar)) //undefined
+
+//step 11
+alert("Hello" + " Nghi" + ", " + "welcome to JavaScript class!")
+
+
+
+
+
+
+
 
